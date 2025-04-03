adca63a6febc75577f72a1687c43fcc61a555983
jhonathanANTUNES <jhony_r8@outlook.com>
2025-04-02 22:02:39 -0300

    Curso em video AULA 7
    Codigos do EP. 07

---
+++ aula 07.py
@@ -0,0 +1,20 @@
+metros = float(input('digite um valor em metros:'))
+centimetro = metros * 100
+milimetro = metros * 1000
+print(f"{metros} metros é igual a {centimetro:.2f} centimetros.")
+print(f"{milimetro} milimetro é igual a {milimetro:.2f} milimetro.")
+
+
+#nota1 = float(input("digite a primeira nota do aluno: "))
+#nota2 = float(input("digite a segunda nota do aluno: "))
+#media = (nota1 + nota2) / 2
+#print(f' a media do aluno é: {media:.2f}')
+
+#numero = float(input('digite um numero: '))
+#dobro = numero * 2
+#triplo = numero * 3
+#raiz_quadrada = numero ** 0.5
+
+#print(f'o dobro de {numero} é {dobro}.')
+#print(f'o triplo de {numero} é {triplo}. ')
+#print(f' a raiz quadrada de {numero} é {raiz_quadrada:.2f}.')
